37038e392099b489a1febc65c1cfe9d3eaad82dc
Romain <[r.cappellotti@gmail.com]>
2025-04-19 14:38:01 +1000

    removing comment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(assetsPath));
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
-// app.get("/favicon.ico", (req, res) => res.status(204)); 
+app.get("/favicon.ico", (req, res) => res.status(204)); 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
 
