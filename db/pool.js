85605fed4a2b83733e6dcdc84e7ad1d92424daa5
Romain <[r.cappellotti@gmail.com]>
2025-04-19 12:27:54 +1000

    connecting railway database

--- db/pool.js
+++ db/pool.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//   host: process.env.DB_HOST, // or wherever the db is hosted
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT // The default port
+// });
+
 module.exports = new Pool({
-  host: process.env.DB_HOST, // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT // The default port
+  connectionString: process.env.DATABASE_URL,
 });
--- db/populatedb.js
+++ db/populatedb.js
@@ -17,7 +17,7 @@ INSERT INTO messages (message, username, date) VALUES ('Hello World!', 'Charles'
 async function main() {
     console.log("seeding...");
     const client = new Client({
-        connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        connectionString: process.env.DATABASE_URL,
     });
     await client.connect();
     await client.query(SQL);
