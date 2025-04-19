7cd754192d940c5219fd9a99268e7fe8de3b5f28
Romain <[r.cappellotti@gmail.com]>
2025-04-19 14:40:35 +1000

    commenting file

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-const { Client } = require("pg");
+// require('dotenv').config();
+// const { Client } = require("pg");
 
-const now = new Date();
-const SQL = `
-CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    message TEXT,
-    username TEXT,
-    date TEXT
-);
+// const now = new Date();
+// const SQL = `
+// CREATE TABLE IF NOT EXISTS messages (
+//     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//     message TEXT,
+//     username TEXT,
+//     date TEXT
+// );
 
-INSERT INTO messages (message, username, date) VALUES ('Hi there!', 'Amando', '${now}');
-INSERT INTO messages (message, username, date) VALUES ('Hello World!', 'Charles', '${now}');
-`;
+// INSERT INTO messages (message, username, date) VALUES ('Hi there!', 'Amando', '${now}');
+// INSERT INTO messages (message, username, date) VALUES ('Hello World!', 'Charles', '${now}');
+// `;
 
-async function main() {
-    console.log("seeding...");
-    const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-    });
-    await client.connect();
-    await client.query(SQL);
-    await client.end();
-    console.log("done");
-};
+// async function main() {
+//     console.log("seeding...");
+//     const client = new Client({
+//         connectionString: process.env.DATABASE_URL,
+//     });
+//     await client.connect();
+//     await client.query(SQL);
+//     await client.end();
+//     console.log("done");
+// };
 
-main();
+// main();
