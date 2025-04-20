17ad8291ea1e7b0a64bc26abd9ad8546bef51fdb
Romain <[r.cappellotti@gmail.com]>
2025-04-20 13:50:43 +1000

    modifying PORT

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const { indexRouter } = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 const path = require("node:path");
 const { body, validationResult } = require("express-validator");
-
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.get("/favicon.ico", (req, res) => res.status(204)); 
