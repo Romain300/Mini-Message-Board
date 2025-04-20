4251ffd57b6fa550883f605094aadc0d7b0e734d
Romain <[r.cappellotti@gmail.com]>
2025-04-20 13:12:35 +1000

    troobleshoting

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Railway!");
